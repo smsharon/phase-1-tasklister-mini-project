24f464fd631fcbe683dc121f85c5f8de176d6d1a
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-21 16:11:59 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  function handleFormSubmission(event){
+    event.preventDefault();
+
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value;
+    const taskUser = document.getElementById('task-user').value;
+    const taskDuration = document.getElementById('task-duration').value;
+    const taskDateDue = document.getElementById('task-date-due').value;
+
+    const newTaskElement = document.createElement('li');
+    newTaskElement.innerHTML = `
+    <strong>${taskDescription}</strong> (User: ${taskUser}, Duration: ${taskDuration}, Due Date: ${taskDateDue}) `;
+
+      //delete function to remove tasks
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Remove';
+      deleteButton.addEventListener('click', () => {
+        newTaskElement.remove();
+      });
+      newTaskElement.appendChild(deleteButton);
+
+      const taskList = document.getElementById('tasks');
+      taskList.appendChild(newTaskElement);
+      taskInput.value = '';
+      document.getElementById('task-user').value = '';
+      document.getElementById('task-duration').value = '';
+      document.getElementById('task-date-due').value = '';
+
+
+  }
+  const createTaskForm = document.getElementById('create-task-form');
+  createTaskForm.addEventListener('submit', handleFormSubmission);
 });
